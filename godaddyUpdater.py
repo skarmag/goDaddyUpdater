a48507938c69b16988550b255a0b2a64457d55ee
skarmag <magnus.skarstein@gmail.com>
2021-09-21 20:33:25 +0200

    init

---
+++ godaddyUpdater.py
@@ -0,0 +1,51 @@
+import requests
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='App for changing IP address of godaddy domains')
+parser.add_argument('domain_name', type=str, help='domain name to be changed')
+args = parser.parse_args()
+
+domain = args.domain_name
+key = 'xxxx' #change this to godaddy key
+secret = 'xxxx' #change this to godaddy secret
+dns_record_type = 'A' #change this if you want to update 
+dns_record_name = '@' #other record types / names
+
+headers = {"Authorization" : "sso-key {}:{}".format(key, secret), "accept": "application/json", "Content-Type": "application/json"}
+url = "https://api.godaddy.com/v1/domains/{}/records/{}/{}".format(domain,dns_record_type,dns_record_name)
+
+
+current_ip = requests.get('https://api.ipify.org').text
+r = requests.get(url,  headers=headers)
+r_json = json.loads(r.text)
+godaddy_ip = r_json[0]['data']
+print('My public IP address is: {}, {} IP is {}'.format(current_ip, domain, godaddy_ip))
+
+update_template = [{
+    "data": current_ip,
+    "name": "@",
+    "port": 65535,
+    "priority": 0,
+    "service": "string",
+    "ttl": 3600,
+    "type": "A",
+    "weight": 1
+    }]
+    
+payload = json.dumps(update_template)  
+
+def update_ip():
+    print("Changing ip on {} to {}...".format(domain, current_ip))
+    p = requests.put(url, headers=headers, data=payload)
+    if p.status_code == 200:
+        print("Success!")
+    else:
+        print("Request failed")
+
+
+if current_ip != godaddy_ip:
+    print("IP address is different, changing ip")
+    update_ip()
+else:
+    print("IP address is equal, bailing out")
